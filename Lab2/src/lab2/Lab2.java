b3037766d3036c3f5d6884f96db180cb04134174
Lance Nathaniel Davis <lndavis6@ncat.edu>
2018-08-27 15:48:58 -0400

    Final project

--- Lab2/src/lab2/Lab2.java
+++ Lab2/src/lab2/Lab2.java
@@ -1,21 +1,65 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Lance Davis
+ * August 27, 2018
+ * Lab 2
  */
 package lab2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  *
  * @author lndavis6
  */
 public class Lab2 {
-
+    static String[] records;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Please enter the filename:");
+        java.util.Scanner keyboard = new java.util.Scanner(System.in);
+        String filename = keyboard.next();
+        java.util.Scanner entei = new java.util.Scanner(filename);
+        int currentIndex = 0;
+        try{
+            Scanner reader = new Scanner(new File(args[0]));
+        }
+        catch (FileNotFoundException ex) {
+            System.err.println("Failed to read input file.");
+        }
+        while(entei.hasNext()){
+            String [] line = entei.nextLine().split(",");
+            verify(line);
+            String firstName = line[0];
+            String lastName = line[1];
+            String age = line[2];
+            String gender = line[3];
+            String phoneNumber = line[4];
+            String email = line[5];
+            
+            records[currentIndex] = String.format("%-20s%-20s%-10s%10d%20d%-20s", firstName, lastName, gender, age, phoneNumber, email);
+            
+            currentIndex++;
+        
+        }
+        for(int i = 0; i <= currentIndex; i++){
+            System.out.println(records[i]);
+        }
+            
+        
+        
     }
+
+    public static void verify(String[] line) {
+            for(int i = 0; i <= line.length; i++){
+            
+            }
+            
+    }
+
+  
     
 }
